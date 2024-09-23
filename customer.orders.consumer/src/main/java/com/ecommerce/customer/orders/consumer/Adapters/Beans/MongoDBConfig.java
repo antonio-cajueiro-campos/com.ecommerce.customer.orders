d0ea283b68794f50ca890f6474f2cf6ffc2ef783
antonio-cajueiro-campos <kimeesan5@gmail.com>
2024-09-23 16:56:06 -0300

    add concern majority

--- customer.orders.consumer/src/main/java/com/ecommerce/customer/orders/consumer/Adapters/Beans/MongoDBConfig.java
+++ customer.orders.consumer/src/main/java/com/ecommerce/customer/orders/consumer/Adapters/Beans/MongoDBConfig.java
@@ -1,11 +1,16 @@
 package com.ecommerce.customer.orders.consumer.Adapters.Beans;
 
+import com.mongodb.ReadConcern;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.model.IndexOptions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.bson.Document;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,11 +18,19 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class MongoDBConfig {
 
-    private final MongoTemplate mongoTemplate;
+    private final MongoClient mongoClient;
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory, MongoConverter converter) {
+        var mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
+        mongoTemplate.setWriteConcern(WriteConcern.MAJORITY);
+        mongoTemplate.getDb().withReadConcern(ReadConcern.MAJORITY);
+        return mongoTemplate;
+    }
 
     @Bean
     public Void createTTLIndex() {
-        var database = mongoTemplate.getDb();
+        var database = mongoClient.getDatabase("orders");
 
         database.getCollection("orders").createIndex(
                 new Document("createdAt", 1),
